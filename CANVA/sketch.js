9e0902698323dde5af12c76d0c7b427ff98e44f8
sd99025 <125955516+A111070037@users.noreply.github.com>
2023-11-29 16:15:27 +0800

    mh

--- CANVA/sketch.js
+++ CANVA/sketch.js
@@ -5,5 +5,15 @@ function setup() {
 }
 function draw() {
     // put drawing code here
-    
+    r = random{255};
+    g = random{255};
+    b = rabdom{255};
+    storkeColor = color(r,g,b);
+    x1 = width/2;
+    y2 = height/2;
+    x2 = random(0,width);
+    y2 = random(0,height/2);
+    strok(strokeColor);
+    strokeWeight(8);
+    line(x1,y1,x2,y2);
 }
