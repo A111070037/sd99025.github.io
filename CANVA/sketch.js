0eb7b52721c0257704e555c142aa1d700463858e
sd99025 <125955516+A111070037@users.noreply.github.com>
2023-12-06 15:53:39 +0800

    Update sketch.js

--- CANVA/sketch.js
+++ CANVA/sketch.js
@@ -1,41 +1,18 @@
 function setup() {
     // put setup code here
-    createCanvas(400, 400);
+    createCanvas(400, 300);
     background(0);
-    angleMode(DEGREES);
-
-    fill(255);
-    text("輸入多邊形的邊數.", 280, 345); 
-
-    inputInt = createInput();
-    inputInt.position(290, 360);
-    inputInt.size(70);
-
-    buttonSubmit = createButton("輸入");
-    buttonSubmit.position(360, 360);
-    buttonSubmit.mousePressed(drawPolygon);
-    
-    stroke('yellow')
+    strokeColor = 'yellow';
+    stroke(strokeColor);
     noFill();
+    x1 = weigh/3;
+    y1 = height*4/5;
+    x2 = weigh/3*2;
+    y2 = height*4/5;
 }
-function drawPolygon() {
+
+function draw() {
     // put drawing code here
     background(0);
-
-    x = width*0.5;
-    y = height*0.5;
-    var numEdges = int(inputInt.value());
-    r = 150;
-    d = 180;
-
-    beginShape();
-    for (i=0; i<=numEdges; i++) {
-        sx = x + r*sin(d);
-        sy = y + r*cos(d);
-        vertex(sx, sy);
-        d += (360/numEdges);
-    }
-    endShape();
-
-    text("輸入多邊形的邊數.", 280, 345);
+    triangle(x1, y1, x2, y2, mouseX, mouseY);
 }
